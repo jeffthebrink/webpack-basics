8216f51c477669d055eff5cabd294c072a3e0f7a
Jeff Brinkley <jbrinkley@churchcommunitybuilder.com>
2019-02-15 14:41:47 -0700

    first webpack build

--- src/AsyncTitle.js
+++
@@ -1,9 +0,0 @@
-import { asyncComponent } from 'react-async-component';
-
-const AsyncTitle = asyncComponent({
-  resolve: () => import('./Title').then(module => module.Title),
-});
-
-export {
-  AsyncTitle,
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { AsyncTitle as Title } from './AsyncTitle';
-
-ReactDOM.render(
-  (
-    <Title text="Is this thing on?" />
-  ),
-  document.getElementById('root'),
-);
+console.log('Hello world!');
